da4972b67e508450d05a983701e9f375921a6c53
raphaelgri <raphaelgriffo@hotmail.com>
2019-01-25 15:31:02 -0200

    Finalização da função de divisão para validação
    Signed-off-by: raphaelgri <raphaelgriffo@hotmail.com>

--- classes/validation.py
+++ classes/validation.py
@@ -2,26 +2,28 @@
 
 import csv
 
-def validation_files_creation(number_of_slices, validation_slice, learning_slice):
+def validation_files_creation(number_of_slices, validation_slice):
 
-    with open('../database/Tabela_TEST.csv') as csv_file:
+    with open('../database/Tabela_TEST.csv') as csv_file:  # conta a quantidade de linhas no arquivo original
         csv_reader = csv.reader(csv_file, delimiter=';')
         reader_line_count = 0
         writer_line_count = 0
         for row in csv_reader:
             reader_line_count += 1
 
-    with open('../database/Tabela_TEST.csv') as csv_file:   #escreve tabela de validação
+    with open('../database/Tabela_TEST.csv') as csv_file:  # escreve tabela de validação
         csv_reader = csv.reader(csv_file, delimiter=';')
 
         with open('../database/validation_tab.csv', mode='w') as validation_tab:
             validation_tab = csv.writer(validation_tab, delimiter=';', quotechar='"', quoting=csv.QUOTE_NONE, escapechar='\\')
             for row2 in csv_reader:
                 writer_line_count += 1
-                if writer_line_count <= (reader_line_count / 5):
+                if writer_line_count <= (reader_line_count / number_of_slices)*validation_slice and writer_line_count > (reader_line_count / number_of_slices)*(validation_slice-1):
                     validation_tab.writerow([row2[0],row2[1]])
 
     writer_line_count = 0 #RESETA ESCRITA
+
+    # criar csv com os textos de aprendizado
 
     with open('../database/Tabela_TEST.csv') as csv_file:  # escreve tabela de aprendizagem
         csv_reader = csv.reader(csv_file, delimiter=';')
@@ -31,21 +33,30 @@ def validation_files_creation(number_of_slices, validation_slice, learning_slice
                                         escapechar='\\')
             for row2 in csv_reader:
                 writer_line_count += 1
-                if writer_line_count > (reader_line_count / 5):
+                if writer_line_count > (reader_line_count / number_of_slices)*validation_slice and writer_line_count <= (reader_line_count / number_of_slices)*validation_slice:
                     training_tab.writerow([row2[0], row2[1]])
 
-validation_files_creation(1,1,1)
 
-#criar csv com os textos de validação
-#definir dados lidos pelo main.py (enviando caminho do .csv de treinamento)
-#leer dados da base alimentada pelo sistema principal
-#validar dados com a base de validação (receber score = taxa de verdadeiros positivos)
-#enviar score e um csv com as probabilidades e os alfas para o firefly
-#por x iterações:
-    #modificar alfas na tabela
-    #leer dados da base alimentada pelo sistema principal
-    #validar dados com a base de validação (receber score = taxa de verdadeiros positivos)
-    #enviar score e um csv com as probabilidades e os alfas para o firefly
+
+
+# definir dados lidos pelo main.py (enviando caminho do .csv de treinamento)
+# leer dados da base alimentada pelo sistema principal
+# validar dados com a base de validação (receber score = taxa de verdadeiros positivos)
+# enviar score e um csv com as probabilidades e os alfas para o firefly
+# por x iterações:
+    # modificar alfas na tabela
+    # leer dados da base alimentada pelo sistema principal
+    # validar dados com a base de validação (receber score = taxa de verdadeiros positivos)
+    # enviar score e um csv com as probabilidades e os alfas para o firefly
 
 # aaaaa
+
+#funcao principal de validacao
+
+def main_validation():
+
+    validation_files_creation(5, 1)
+
+
+main_validation()
 

f325a5462972cf6c0c9c4582a51b3dbc911acaf3
raphaelgri <raphaelgriffo@hotmail.com>
2019-03-07 23:47:30 -0300

    Criação do módulo de relatório para validação / testes.

--- classes/validation.py
+++ classes/validation.py
@@ -4,6 +4,8 @@ import csv
 from classes.text import Text
 from classes.db import DB
 import random
+import datetime
+import sys
 
 def brilho(firefly):
     b = 0
@@ -11,7 +13,7 @@ def brilho(firefly):
         b += firefly[i]
     return b
 
-def validation_files_creation(number_of_slices, validation_slice):
+def validation_files_creation(number_of_slices, validation_slice): #numero de divisões do arquivo de entrada e divisão selecionada para validação
 
     with open('../database/Tabela_TEST.csv') as csv_file:  # conta a quantidade de linhas no arquivo original
         csv_reader = csv.reader(csv_file, delimiter=';')
@@ -51,30 +53,35 @@ def validation_files_creation(number_of_slices, validation_slice):
 # definir dados lidos pelo main.py (enviando caminho do .csv de treinamento)
 # leer dados da base alimentada pelo sistema principal
 
-#construir  texto baseado no arquivo de validação
+#construir texto baseado no arquivo de validação
 
-def validation_text_comparison():
+def validation_text_comparison(data_validation_source):
     DB_V = DB(path='../database/',debug=True)
-
-    with open('../database/training_tab.csv',  encoding='utf-8-sig') as csv_file:  # conta a quantidade de linhas no arquivo original
+    now = datetime.datetime.now()
+    with open(data_validation_source,  encoding='utf-8-sig') as csv_file:  # conta a quantidade de linhas no arquivo original
         csv_reader = csv.reader(csv_file, delimiter=';')
-        for row in csv_reader:
-            t = Text(str.split(str.upper(row[1])), row[0])
-            prob = 0.5
-            if t:
-                t.build_phrases(3) #construir frases
-                for p in t.phrases:
-                    prob = prob * (1- DB_V.get_phrase_prob(p)) # busca a probabilidade associada à frase e calcula probabilidade do texto
-                    prob = 1 - prob
-                print("====")
-                print(row)
-                print(t.probability)
-                print(row[0])
-                print(prob)
-                del t
-            else:
-                return -100
 
+        with open("report_%d_%d_%d.out" %now.day %now.month %now.year, "w") as report: #abre arquivo de relatório
+            orig_stdout = sys.stdout # guarda saida padrão
+            sys.stdout = report # troca saida padrão por relatório
+            for row in csv_reader:
+                t = Text(str.split(str.upper(row[1])), row[0])
+                prob = 0.5
+                if t:
+                    t.build_phrases(3) #construir frases
+                    for p in t.phrases:
+                        prob = prob * (1- DB_V.get_phrase_prob(p)) # busca a probabilidade associada à frase e calcula probabilidade do texto
+                        prob = 1 - prob
+                    print("====")
+                    print(row)
+                    print(t.probability)
+                    print(row[0])
+                    print(prob)
+                    del t
+                else:
+                    return -100
+        sys.stdout = orig_stdout    # reseta saída
+        report.close()  #fechar arquivo de relatório
 
 
 #calcular veracidade do texto
@@ -95,9 +102,10 @@ def validation_text_comparison():
 
 def main_validation():
 
+
     validation_files_creation(5, 2)
-    validation_text_comparison()
+    validation_text_comparison('../database/training_tab.csv')
 
 
-# main_validation()
+main_validation()
 

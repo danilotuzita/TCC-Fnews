b36252ad084aece25c04553031d17e7a6066fa3c
Danilo Sanchez Tuzita <danilo_st@hotmail.com>
2019-05-26 00:51:00 -0300

    experimento 1 finalizado; setup do experimento 2

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from classes.util import *
 # params setup
 _slice = 0
 p_delta = 0
-experiment = '1'
+bound = 0.25
+experiment = '0'
 phrase_size = 2
 
 # path setup
@@ -140,11 +141,14 @@ def test(ff=0, upper_bound=0.75, lower_bound=0.25):
         print("Plus Delta : ", p_delta)
         print("Upper Bound: ", upper_bound)
         print("Lower Bound: ", lower_bound)
-        print("True Positive: " + str(truenews_count) + ", Found: " + str(true_positive) +
-                     " (" + str(true_positive * 100 / truenews_count) + "%)")
-
-        print("True Negative: " + str(fakenews_count) + ", Found: " + str(true_negative) +
-                     " (" + str(true_negative * 100 / fakenews_count) + "%)")
+        print(
+            "True Positive: " + str(truenews_count) + ", Found: " + str(true_positive) +
+            " (" + str(true_positive * 100 / truenews_count) + " %)"
+        )
+        print(
+            "True Negative: " + str(fakenews_count) + ", Found: " + str(true_negative) +
+            " (" + str(true_negative * 100 / fakenews_count) + " %)"
+        )
         print("Assertivity  : ", assertivity)
         print(row_number, " lines tested")
         result_file.at[result_file_row, 'True News'] = truenews_count
@@ -157,79 +161,50 @@ def test(ff=0, upper_bound=0.75, lower_bound=0.25):
 
 
 def ex_1():
-    global phrase_size, _slice, p_delta, result_file, result_file_row
-    result_file = pandas.read_csv('experiments/1/experiment.csv')
+    global phrase_size, _slice, p_delta, result_file, result_file_row, experiment
+    experiment = '1'
+    result_filename = 'experiment_1.csv'
+    result_file = pandas.read_csv('experiments/' + experiment + '/' + result_filename)
     for i, row in result_file.iterrows():
         if pandas.isna(row['True News']):
             result_file_row = i
             phrase_size = int(row['Words'])
             _slice = int(row['Slice'])
             p_delta = int(row['Plus Delta'])
+            print(
+                "Slice      : ", _slice, '\n'
+                "Phrase Size: ", phrase_size, '\n'
+                "Plus Delta : ", p_delta
+            )
             load_all()
             test()
-            if not isfile(csv_path + str(phrase_size) + '/' + "validation.dbh"):
-                _dbh = get_all_phrases_prob(csv_path + validation_filename, db, phrase_size)
-                _dbh.to_file(csv_path + str(phrase_size) + '/', "validation.dbh")
-        result_file.to_csv('experiments/1/experiment.csv')
+            result_file.to_csv('experiments/' + experiment + '/' + result_filename)
+
+
+def ex_2():
+    global phrase_size, _slice, p_delta, result_file, result_file_row, bound, experiment
+    experiment = '2'
+    result_filename = 'experiment.csv'
+    result_file = pandas.read_csv('experiments/' + experiment + '/' + result_filename)
+    for i, row in result_file.iterrows():
+        if pandas.isna(row['True News']):
+            result_file_row = i
+            phrase_size = int(row['Words'])
+            _slice = int(row['Slice'])
+            p_delta = int(row['Plus Delta'])
+            bound = float(row['Bounds'])
+            print(
+                "Slice      : ", _slice, '\n'
+                "Phrase Size: ", phrase_size, '\n'
+                "Plus Delta : ", p_delta, '\n'
+                "Bounds     : ", bound
+            )
+            load_all()
+            test(
+                upper_bound=0.5 + bound,
+                lower_bound=0.5 - bound
+            )
+        result_file.to_csv('experiments/' + experiment + '/' + result_filename)
 
 
 ex_1()
-
-# from classes.validation import validation_files_creation
-
-# for i in range(1, 6):
-#     validation_files_creation(5, i, 'experiments/cross validation files/10700/LIAR_1_10700.csv',
-#                               'experiments/cross validation files/10700/' + str(i))
-
-
-
-"""
-ff = np.array([0.28126, 0.26142, 0.0035938, 0.39035, 0.063372]) + 1
-dbh = DbHandler()
-dbh.from_file(experiment_path, 'dbhExperimento_FULL.txt')
-
-quinto = len(dbh.texts) / 5
-
-truenews_count = 0
-fakenews_count = 0
-
-true_positive = 0
-true_negative = 0
-
-upper_bound = 0.75
-lower_bound = 0.25
-
-for i, text in enumerate(dbh.texts):
-    probs = []
-    for phrase in text.phrases:
-        probs.append(phrase)
-    [aaa, bbb] = get_used_clusters(probs, 5)
-    ground_truth = text.real_prob
-    text_prob = calc_prob(aaa, ff)
-    if i > quinto:
-        break
-
-    if ground_truth >= upper_bound:
-        if text_prob >= upper_bound:
-            true_positive += 1
-            correct_classification = True
-        truenews_count += 1
-
-    if ground_truth <= lower_bound:
-        if text_prob <= lower_bound:
-            true_negative += 1
-            correct_classification = True
-        fakenews_count += 1
-
-
-print("True Positive: " + str(truenews_count) + ", Found: " + str(true_positive) +
-             " (" + str(true_positive * 100 / truenews_count) + "%)\n")
-
-print("True Negative: " + str(fakenews_count) + ", Found: " + str(true_negative) +
-             " (" + str(true_negative * 100 / fakenews_count) + "%)\n")
-
-print("General Assertivity: " + str(
-    (true_positive + true_negative) * 100 /
-    (truenews_count + fakenews_count)
-) + '\n')
-"""
